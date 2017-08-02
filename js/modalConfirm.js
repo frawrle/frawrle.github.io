95ebad5cba47f6ea3ee89d2da2e6d66877e3dff2
frawrle <frawrle.ptrapile@gmail.com>
2017-08-02 23:39:18 +0900

    Update modalConfirm.js

--- js/modalConfirm.js
+++ js/modalConfirm.js
@@ -6,7 +6,7 @@
 
 $(function(){
 	var modal = $(".millibox");//モーダルウインドウのクラス
-	var opacity = 0.9;//モーダル背景の透明度
+	var opacity = 0.1;//モーダル背景の透明度
 	var button = $(".button");//モーダル解除ボタンのクラス
 	var limit = 120;//Cookieの有効期限(分)
 	var cookie = $.cookie("modal");
