3380d78c45a46221eeb2464076b0fa87f7b5ad52
frawrle <frawrle.ptrapile@gmail.com>
2018-02-19 21:16:26 +0900

    Create yukakiriwrite.js

---
+++ js/yukakiriwrite.js
@@ -0,0 +1,28 @@
+//HTMLが完全に読み込み終わってからdraw()を実行する
+window.onload = function () {
+    draw();
+};
+
+function draw () {
+    var canvas = document.getElementById('mycanvas');
+    if (!canvas || !canvas.getContext) return false;
+    var ctx = canvas.getContext('2d');
+
+    //ここからctx（Canvasオブジェクト）に対しての描画処理を書く
+    // 任意の文字をランダム描画
+    ctx.globalAlpha = 0.5;
+    var kiri = ['結月ゆかり','東北きりたん','結月きりたん','終末','崩壊'];
+
+    for (var i = 0; i < 3; i++) {
+        var x = Math.floor(Math.random() * 900);
+        var y = Math.floor(Math.random() * 600);
+        var r = Math.floor(Math.random() * 45);
+        var s = kiri[Math.floor(Math.random() * 5)];
+
+        ctx.font = "italic "+r+"px Verdana"; // 文字フォント指定
+        ctx.fillStyle = "rgb(255,255,255)"; // 塗りつぶしスタイル指定
+        ctx.fillText(s, x, y, 200); // (文字,始点x,y,最大横幅)に文字を描画
+    }
+
+   
+}
